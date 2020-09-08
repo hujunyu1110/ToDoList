d45353286adda6082106fd35406736c8dd837d72
Your Name <you@example.com>
2020-09-08 15:27:32 +0800

    *

---
+++ js/commom.js
@@ -0,0 +1,3 @@
+/* XXX: url:port */
+var Url = "182.92.231.61:8082";
+var LocalUrl = "localhost:8082";
---
+++ js/unit.js
@@ -0,0 +1,18 @@
+var Unit = {
+    /*
+    Get: function (url,callBack) {
+        var xhr = new XMLHttpRequest(); //第一步：新建对象
+        xhr.open('GET', url, true); //第二步：打开连接  将请求参数写在url中 
+        xhr.send(); //第三步：发送请求  将请求参数写在URL中
+
+         //取数据后的处理程序
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                callBack(xhr);
+                var res = xhr.responseText; //获取到json字符串，解析
+            }
+        };
+    }
+    */
+}
